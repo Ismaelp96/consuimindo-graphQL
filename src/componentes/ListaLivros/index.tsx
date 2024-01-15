546be6b607ebaa6528ee2d57eac2b6098f028721
ismaelp96 <ismael6991@live.com>
2024-01-15 13:11:31 -0300

    Cirando e definindo reactiveVars

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useReactiveVar } from '@apollo/client';
 
 import { ICategoria } from '../../interfaces/ICategoria';
 import CardLivro from '../CardLivro';
 import './ListaLivros.css';
 import { AbBotao, AbCampoTexto } from 'ds-alurabooks';
 import { useLivros } from '../../graphql/livros/hooks';
+import { livrosVar } from '../../graphql/livros/state';
 
 interface ListaLivrosProps {
   categoria: ICategoria;
@@ -13,7 +15,15 @@ interface ListaLivrosProps {
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
   const [textoBusca, setTextoBusca] = useState('');
 
+  const livros = useReactiveVar(livrosVar);
+  console.log('livros =>', livros);
+
   const { data, refetch } = useLivros(categoria);
+
+  if (data?.livros) {
+    livrosVar(data.livros);
+  }
+
   const buscarLivros = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (textoBusca) {
